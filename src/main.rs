4f69d3e7393793eafc8d862797676e5a76577bee
Alexanderdunlop <alexjamesdunlop@gmail.com>
2023-11-09 22:53:47 +0000

    print statements

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,99 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
-fn main() {
+fn print_input() {
     println!("What is your name?");
-    let mut name: String = String::new();
-    let greeting: &str = "Nice to meet you";
+    let mut name = String::new();
+    let greeting = "Nice to meet you";
     io::stdin().read_line(&mut name)
         .expect("Didn't Receive Input");
     println!("Hello {}! {}", name.trim_end(), greeting);
+}
+
+fn print_parsed_str() {
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age = "26";
+    let mut age: u32 = age.trim().parse()
+        .expect("Age wasn't assigned a number");
+    age = age + 1;
+    println!("I'm {} and I want ${}", age, ONE_MIL);
+}
+
+fn print_integer() {
+    // Unsigned integer : u8, u16, u32, u64, u128, usize
+    // Signed integer : i8, i16, i32, i64, i128, isize
+    println!("Max u32 : {}", u32::MAX);
+    println!("Max u64 : {}", u64::MAX);
+    println!("Max u128 : {}", u128::MAX);
+    println!("Max usize : {}", usize::MAX);
+    println!("Max f32 : {}", f32::MAX);
+    println!("Max f64 : {}", f64::MAX);
+}
+
+fn print_precision_and_operators() {
+    let _unused_var = true;
+    let _is_true = true;
+    let _my_grade = 'A';
+
+    // 32 bit float will have 6 digits of precision
+    let num_1: f32 = 1.111111111111111;
+    println!("f32 : {}", num_1 + 0.111111111111111);
+    // 64 bit float will have 14 digits of precision
+    let num_2: f64 = 1.111111111111111;
+    println!("f64 : {}", num_2 + 0.111111111111111);
+
+    let num_3: u32 = 5;
+    let num_4: u32 = 4;
+    println!("5 + 4 = {}", num_3 + num_4);
+    println!("5 - 4 = {}", num_3 - num_4);
+    println!("5 * 4 = {}", num_3 * num_4);
+    println!("5 / 4 = {}", num_3 / num_4);
+    println!("5 % 4 = {}", num_3 % num_4);
+
+    let random_num = rand::thread_rng().gen_range(1..101);
+    println!("Random : {}", random_num);
+}
+
+fn print_if_statements() {
+    let age = 8;
+    if (age >= 1) && (age <= 18){
+        println!("Important Birthday");
+    } else if (age == 21) || (age == 50){
+        println!("Important Birthday");
+    } else if age >= 65 {
+        println!("Important Birthday");
+    } else {
+        println!("Not an Important Birthday");
+    }
+}
+
+fn print_ternary() {
+    let mut my_age = 27;
+    let can_vote = if my_age >= 18{
+        true
+    } else {
+        false
+    };
+    println!("Can Vote : {}", can_vote);
+}
+
+fn print_match_statements() {
+    let age2 = 8;
+    match age2 {
+        1..=18 => println!("Important Birthday"),
+        21 | 50 => println!("Important Birthday"),
+        65..=i32::MAX => println!("Important Birthday"),
+        _ => println!("Not an Important Birthday"),
+    };
+}
+
+fn main() {
+    let my_age = 18;
+    let voting_age = 18;
+    match my_age.cmp(&voting_age){
+        Ordering::Less => println!("Can't Vote"),
+        Ordering::Greater => println!("Can Vote"),
+        Ordering::Equal => println!("You gained the right to vote"),
+    }
 }
